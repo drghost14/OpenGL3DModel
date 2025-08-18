a89018a95c0d4091f9d9d5a7368e204657bf9e22
DRG_Games <dr.ghost1419@gmail.com>
2025-08-18 21:27:24 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -19,123 +19,6 @@ using std::cout, std::string, std::ifstream, std::ostringstream, std::vector, gl
     glm::vec2, glm::vec3, glm::mat4, glm::normalize, glm::lookAt, glm::cross, glm::radians,
     glm::cos, glm::sin, glm::perspective, glm::translate;
 
-enum TEXTURE_PARAMETER
-{
-    TEXTURE_LINEAR  = 1,
-    TEXTURE_NEAREST = 2
-};
-
-class CameraClass
-{
-public:
-    CameraClass  () {}
-    ~CameraClass () {}
-
-    void Initialize ()
-    {
-        cameraPos   = vec3 ( 0.0f,  0.0f,  3.0f);
-        cameraUp    = vec3 ( 0.0f,  1.0f,  0.0f);
-        cameraFront = vec3 ( 0.0f, -1.0f,  0.0f);
-    }
-
-    void Update     ()
-    {
-        HandleInput ();
-        cameraView = lookAt (cameraPos, cameraPos + cameraFront, cameraUp);
-    }
-
-    mat4 GetCameraView () const
-    {
-        return cameraView;
-    }
-private:
-    vec3 cameraPos;
-    vec3 cameraUp;
-    vec3 cameraFront;
-    mat4 cameraView;
-
-    const float cameraMovementSpeed = 0.5f;
-    const float cameraRotationSpeed = 45.0f;
-
-    float cameraYaw   = -90.0f;
-    float cameraPitch =  0.0f;
-    // private functions
-    void HandleInput ()
-    {
-        float dt = GetFrameTime ();
-        HandleMovement  (dt);
-        HandleRotation  (dt);
-        UpdateDirection ();
-    }
-    void HandleMovement (float dt) 
-    {
-        if (IsKeyDown (KEY_W)) cameraPos += cameraFront * cameraMovementSpeed * dt;
-        if (IsKeyDown (KEY_S)) cameraPos -= cameraFront * cameraMovementSpeed * dt;
-        if (IsKeyDown (KEY_A)) cameraPos -= normalize (cross (cameraFront, cameraUp)) * cameraMovementSpeed * dt;
-        if (IsKeyDown (KEY_D)) cameraPos += normalize (cross (cameraFront, cameraUp)) * cameraMovementSpeed * dt;
-    }
-    void HandleRotation (float dt) 
-    {
-        if (IsKeyDown (KEY_RIGHT)) cameraYaw += cameraRotationSpeed * dt;
-        if (IsKeyDown (KEY_LEFT )) cameraYaw -= cameraRotationSpeed * dt;
-        if (IsKeyDown (KEY_UP  )) cameraPitch += cameraRotationSpeed * dt;
-        if (IsKeyDown (KEY_DOWN)) cameraPitch -= cameraRotationSpeed * dt;
-
-        if (cameraPitch >  89.0f) cameraPitch =  89.0f;
-        if (cameraPitch < -89.0f) cameraPitch = -89.0f;
-    }
-
-    void UpdateDirection ()
-    {
-        float tempYaw   = radians (cameraYaw);
-        float tempPitch = radians (cameraPitch);
-
-        vec3 tempFront;
-        tempFront.x = cos (tempYaw  ) * cos (tempPitch);
-        tempFront.y = sin (tempPitch);
-        tempFront.z = sin (tempYaw  ) * cos (tempPitch);
-
-        cameraFront = normalize (tempFront);
-    }
-
-};
-
-class TextureClass
-{
-public:
-    TextureClass  () {}
-    ~TextureClass ()
-    {
-        UnloadTexture (texture);
-    }
-
-    void Initialize (const char *filePath, int textureUnit, TEXTURE_PARAMETER textureParameter)
-    {
-        Image tempImage = LoadImage (filePath);
-        ImageFlipVertical (&tempImage);
-
-        texture = LoadTextureFromImage (tempImage);
-        glActiveTexture (GL_TEXTURE0 + textureUnit);
-        glBindTexture   (GL_TEXTURE_2D, texture.id);
-
-        switch (textureParameter)
-        {
-            case TEXTURE_LINEAR :
-                glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-                glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-                break;
-            case TEXTURE_NEAREST:
-                glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
-                glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-                break;
-        }
-        glGenerateMipmap (GL_TEXTURE_2D);
-        UnloadImage (tempImage);
-    }
-private:
-    Texture2D texture;
-};
-
 // graphics pipeline
 class ShaderClass
 {
@@ -287,111 +170,267 @@ private:
     unsigned int buffer;
 };
 
+
+struct VertexStruct
+{
+    vec3 Position;
+    vec3 Normal;
+    vec2 Uv;
+};
+
+enum TEXTURE_PARAMETER
+{
+    TEXTURE_LINEAR  = 1,
+    TEXTURE_NEAREST = 2
+};
+
+class CameraClass
+{
+public:
+    CameraClass  () {}
+    ~CameraClass () {}
+
+    void Initialize (float fov)
+    {
+        cameraPos        = vec3 ( 0.0f,  0.0f,  3.0f);
+        cameraUp         = vec3 ( 0.0f,  1.0f,  0.0f);
+        cameraFront      = vec3 ( 0.0f, -1.0f,  0.0f);
+        cameraProjection = perspective (radians (fov), (float) WIDTH / (float) HEIGHT, 0.1f, 100.0f);
+    }
+    void Update     ()
+    {
+        HandleInput ();
+        cameraView = lookAt (cameraPos, cameraPos + cameraFront, cameraUp);
+    }
+    mat4 GetCameraView () const
+    {
+        return cameraView;
+    }
+    mat4 GetCameraProjection () const
+    {
+        return cameraProjection;
+    }
+private:
+    vec3 cameraPos;
+    vec3 cameraUp;
+    vec3 cameraFront;
+    mat4 cameraView;
+    mat4 cameraProjection;
+
+    const float cameraMovementSpeed = 5.0f;
+    const float cameraRotationSpeed = 60.0f;
+
+    float cameraYaw   = -90.0f;
+    float cameraPitch =  0.0f;
+    // private functions
+    void HandleInput ()
+    {
+        float dt = GetFrameTime ();
+        HandleMovement  (dt);
+        HandleRotation  (dt);
+        UpdateDirection ();
+    }
+    void HandleMovement (float dt) 
+    {
+        if (IsKeyDown (KEY_W)) cameraPos += cameraFront * cameraMovementSpeed * dt;
+        if (IsKeyDown (KEY_S)) cameraPos -= cameraFront * cameraMovementSpeed * dt;
+        if (IsKeyDown (KEY_A)) cameraPos -= normalize (cross (cameraFront, cameraUp)) * cameraMovementSpeed * dt;
+        if (IsKeyDown (KEY_D)) cameraPos += normalize (cross (cameraFront, cameraUp)) * cameraMovementSpeed * dt;
+        
+        if (IsKeyDown (KEY_SPACE     )) cameraPos += cameraUp * cameraMovementSpeed * dt;
+        if (IsKeyDown (KEY_LEFT_SHIFT)) cameraPos -= cameraUp * cameraMovementSpeed * dt;
+    }
+    void HandleRotation (float dt) 
+    {
+        if (IsKeyDown (KEY_RIGHT)) cameraYaw += cameraRotationSpeed * dt;
+        if (IsKeyDown (KEY_LEFT )) cameraYaw -= cameraRotationSpeed * dt;
+        if (IsKeyDown (KEY_UP  )) cameraPitch += cameraRotationSpeed * dt;
+        if (IsKeyDown (KEY_DOWN)) cameraPitch -= cameraRotationSpeed * dt;
+
+        if (cameraPitch >  89.0f) cameraPitch =  89.0f;
+        if (cameraPitch < -89.0f) cameraPitch = -89.0f;
+    }
+
+    void UpdateDirection ()
+    {
+        float tempYaw   = radians (cameraYaw);
+        float tempPitch = radians (cameraPitch);
+
+        vec3 tempFront;
+        tempFront.x = cos (tempYaw  ) * cos (tempPitch);
+        tempFront.y = sin (tempPitch);
+        tempFront.z = sin (tempYaw  ) * cos (tempPitch);
+
+        cameraFront = normalize (tempFront);
+    }
+
+};
+
+class TextureClass
+{
+public:
+    TextureClass  () {}
+    ~TextureClass ()
+    {
+        UnloadTexture (texture);
+    }
+    void Initialize (const char *filePath, int textureUnit, TEXTURE_PARAMETER textureParameter)
+    {
+        Image tempImage = LoadImage (filePath);
+        ImageFlipVertical (&tempImage);
+
+        texture = LoadTextureFromImage (tempImage);
+        glActiveTexture (GL_TEXTURE0 + textureUnit);
+        glBindTexture   (GL_TEXTURE_2D, texture.id);
+
+        switch (textureParameter)
+        {
+            case TEXTURE_LINEAR :
+                glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+                glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+                break;
+            case TEXTURE_NEAREST:
+                glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+                glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+                break;
+        }
+        glGenerateMipmap (GL_TEXTURE_2D);
+        UnloadImage (tempImage);
+    }
+    int GetTextureUnit ()
+    {
+        return textureUnit;
+    }
+    void BindTexture   ()
+    {
+        glActiveTexture (GL_TEXTURE0 + textureUnit);
+        glBindTexture   (GL_TEXTURE_2D, texture.id);
+    }
+private:
+    Texture2D texture    ;
+    int       textureUnit;
+};
+
+class MeshClass
+{
+public:
+    void PassData   (vector<VertexStruct> &vertex, vector<unsigned int> &index, const char *textureFilePath, int textureUnit)
+    {
+        this -> vertex = vertex;
+        this -> index  = index ;
+        texture.Initialize (textureFilePath, textureUnit, TEXTURE_LINEAR);
+    }
+    void Initialize ()
+    {
+        vao.Initialize ();
+        vao.Bind       ();
+
+        vbo.Initialize (GL_ARRAY_BUFFER, vertex.size () * sizeof (VertexStruct), vertex.data ());
+        ebo.Initialize (GL_ELEMENT_ARRAY_BUFFER, index.size () * sizeof (unsigned int), index.data());
+
+        GLsizei stride = sizeof (VertexStruct);
+        vao.SetData    (0, 3, stride, (void*) (offsetof (VertexStruct, Position)));
+        vao.SetData    (1, 3, stride, (void*) (offsetof (VertexStruct, Normal  )));
+        vao.SetData    (2, 2, stride, (void*) (offsetof (VertexStruct, Uv      )));
+        vao.Unbind     ();
+    }
+    void Draw (ShaderClass &shader)
+    {
+        texture.BindTexture      ();
+        shader.Use               ();
+        shader.SetUniformInteger ("uTexture", texture.GetTextureUnit ());
+        vao.Bind                 ();
+        glDrawElements           (GL_TRIANGLES, index.size (), GL_UNSIGNED_INT, 0);
+        vao.Unbind               ();
+    }
+private:
+    vector<VertexStruct> vertex;
+    vector<unsigned int> index ;
+    VaoClass                vao;
+    BufferClass        vbo, ebo;
+    TextureClass       texture ;
+};
+
+class ModelClass 
+{
+public:
+    void Initialize (const char *modelFilePath, const char *textureFilePath, int textureUnit)
+    {
+        LoadModel3D     (modelFilePath);
+        mesh.PassData   (tempVertex, tempIndex, textureFilePath, textureUnit);
+        mesh.Initialize ();
+        model = mat4    (1.0f);
+    }
+    void Draw       (ShaderClass &shader)
+    {
+        shader.Use            ();
+        shader.SetUniformMat4 ("model", model);
+        mesh.Draw             (shader);
+    }
+private:
+    vector<VertexStruct> tempVertex;
+    vector<unsigned int> tempIndex ;
+    Model                tempModel ;
+    MeshClass            mesh      ;
+    mat4                 model     ;
+
+    void LoadModel3D (const char *modelFilePath)
+    {
+        tempModel = LoadModel (modelFilePath);
+
+        for (int m = 0; m < tempModel.meshCount; m++)
+        {
+            Mesh &src = tempModel.meshes [m];
+            for (int v = 0; v < src.vertexCount; v++)
+            {
+                VertexStruct vertex {};
+                vertex.Position = vec3 (src.vertices [v * 3 + 0],
+                                        src.vertices [v * 3 + 1],
+                                        src.vertices [v * 3 + 2]);
+                if (src.texcoords != 0)
+                {
+                    vertex.Uv = vec2 (src.texcoords      [v * 2 + 0],
+                                    1.0f - src.texcoords [v * 2 + 1]); // image flip
+                }
+                tempVertex.push_back (vertex);
+            }
+            for (int i = 0; i < src.triangleCount * 3; i++)
+            {
+                tempIndex.push_back (static_cast<unsigned int> (src.indices [i]));
+            }
+        }
+        UnloadModel (tempModel);
+    }
+};
+
 int main ()
 {
-    mat4 projection = perspective (radians (45.0f), (float) WIDTH / (float) HEIGHT, 0.1f, 100.0f);
-    mat4 model      = mat4 (1.0f);
-    vec3 modelPos   = vec3 ( 0.0f, -1.0f,  -2.0f);
-    model           = translate (model, modelPos);
-
-    vector<float>       vertex =
-    {// vertices                normals                 tex coords
-        -0.5f, -0.5f,  0.5f,    0.0f,  0.0f,  1.0f,     0.0f, 0.0f, // bottom left  (z+) 0
-         0.5f, -0.5f,  0.5f,    0.0f,  0.0f,  1.0f,     1.0f, 0.0f, // bottom right (z+) 1
-         0.5f,  0.5f,  0.5f,    0.0f,  0.0f,  1.0f,     1.0f, 1.0f, // top right    (z+) 2
-        -0.5f,  0.5f,  0.5f,    0.0f,  0.0f,  1.0f,     0.0f, 1.0f, // top left     (z+) 3
-
-        -0.5f, -0.5f, -0.5f,    0.0f,  0.0f, -1.0f,     0.0f, 0.0f, // bottom left  (z-) 4
-         0.5f, -0.5f, -0.5f,    0.0f,  0.0f, -1.0f,     1.0f, 0.0f, // bottom right (z-) 5
-         0.5f,  0.5f, -0.5f,    0.0f,  0.0f, -1.0f,     1.0f, 1.0f, // top right    (z-) 6
-        -0.5f,  0.5f, -0.5f,    0.0f,  0.0f, -1.0f,     0.0f, 1.0f, // top left     (z-) 7
-
-         0.5f, -0.5f, -0.5f,    1.0f,  0.0f,  0.0f,     0.0f, 0.0f, // bottom left  (x+) 8
-         0.5f, -0.5f,  0.5f,    1.0f,  0.0f,  0.0f,     1.0f, 0.0f, // bottom right (x+) 9
-         0.5f,  0.5f,  0.5f,    1.0f,  0.0f,  0.0f,     1.0f, 1.0f, // top right    (x+) 10
-         0.5f,  0.5f, -0.5f,    1.0f,  0.0f,  0.0f,     0.0f, 1.0f, // top left     (x+) 11
-
-        -0.5f, -0.5f, -0.5f,   -1.0f,  0.0f,  0.0f,     0.0f, 0.0f, // bottom left  (x-) 12
-        -0.5f, -0.5f,  0.5f,   -1.0f,  0.0f,  0.0f,     1.0f, 0.0f, // bottom right (x-) 13
-        -0.5f,  0.5f,  0.5f,   -1.0f,  0.0f,  0.0f,     1.0f, 1.0f, // top right    (x-) 14
-        -0.5f,  0.5f, -0.5f,   -1.0f,  0.0f,  0.0f,     0.0f, 1.0f, // top left     (x-) 15
-
-        -0.5f,  0.5f,  0.5f,    0.0f,  1.0f,  0.0f,     0.0f, 0.0f, // bottom left  (y+) 16
-         0.5f,  0.5f,  0.5f,    0.0f,  1.0f,  0.0f,     1.0f, 0.0f, // bottom right (y+) 17
-         0.5f,  0.5f, -0.5f,    0.0f,  1.0f,  0.0f,     1.0f, 1.0f, // top right    (y+) 18
-        -0.5f,  0.5f, -0.5f,    0.0f,  1.0f,  0.0f,     0.0f, 1.0f, // top left     (y+) 19
-
-        -0.5f, -0.5f,  0.5f,    0.0f, -1.0f,  0.0f,     0.0f, 0.0f, // bottom left  (y-) 20
-         0.5f, -0.5f,  0.5f,    0.0f, -1.0f,  0.0f,     1.0f, 0.0f, // bottom right (y-) 21
-         0.5f, -0.5f, -0.5f,    0.0f, -1.0f,  0.0f,     1.0f, 1.0f, // top right    (y-) 22
-        -0.5f, -0.5f, -0.5f,    0.0f, -1.0f,  0.0f,     0.0f, 1.0f, // top left     (y-) 23
-    };
-
-    vector<unsigned int> index =
-    {
-         0,  1,  2, // (z+)
-         0,  2,  3, // (z+)
-
-         4,  5,  6, // (z-)
-         4,  6,  7, // (z-)
-
-         8,  9, 10, // (x+)
-         8, 10, 11, // (x+)
-
-        12, 13, 14, // (x-)
-        12, 14, 15, // (x-)
-
-        16, 17, 18, // (y+)
-        16, 18, 19, // (y+)
-
-        20, 21, 22, // (y-)
-        20, 22, 23  // (y-)
-    };
-
-    SetConfigFlags (FLAG_WINDOW_RESIZABLE);
-    InitWindow     (WIDTH, HEIGHT, "texture");
+    SetConfigFlags (FLAG_WINDOW_RESIZABLE | FLAG_MSAA_4X_HINT);
+    InitWindow     (WIDTH, HEIGHT, "opengl test window");
+    glViewport     (0, 0, WIDTH, HEIGHT);
     glEnable       (GL_DEPTH_TEST);
-    glDisable      (GL_CULL_FACE);
+    glEnable       (GL_MULTISAMPLE);
 
-    CameraClass   camera;
-    TextureClass texture;
+    ShaderClass shader;
 
-    ShaderClass  shaders;
-    VaoClass         vao;
-    BufferClass vbo, ebo;
+    CameraClass camera;
+    ModelClass  model ;
 
-    camera. Initialize ();
-    texture.Initialize ("data/car.png", 0, TEXTURE_NEAREST);
-
-    shaders.Initialize ("shaders/vertex.glsl", "shaders/fragment.glsl");
-    vao.Initialize ();
-    vao.Bind       ();
-
-    vbo.Initialize (GL_ARRAY_BUFFER, vertex.size () * sizeof (float), vertex.data ());
-    ebo.Initialize (GL_ELEMENT_ARRAY_BUFFER, index.size () * sizeof (unsigned int), index.data ());
-
-    GLsizei stride = sizeof (float) * 8;
-    vao.SetData (0, 3, stride, ((void*) 0));
-    vao.SetData (1, 3, stride, ((void*) ((sizeof (float) * 3))));
-    vao.SetData (2, 2, stride, ((void*) ((sizeof (float) * 6))));
-    vao.Unbind  ();
+    shader.Initialize ("shaders/vertex.glsl", "shaders/fragment.glsl");
+    
+    camera.Initialize (60.0f);
+    model .Initialize ("data/car.glb", "data/car_texture.png", 0);
 
     while (!WindowShouldClose ())
     {
         camera.Update ();
         BeginDrawing  ();
-            glClearColor (245/255.0f, 245/255.0f, 250/255.0f, 1.0f);
-            glClear      (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-            
-            vao.Bind                  ();
-            shaders.Use               ();
-            shaders.SetUniformInteger ("uTexture",   0);
-            shaders.SetUniformMat4    ("model",      model);
-            shaders.SetUniformMat4    ("cameraView", camera.GetCameraView ());
-            shaders.SetUniformMat4    ("projection", projection);
-            glDrawElements            (GL_TRIANGLES, index.size (), GL_UNSIGNED_INT, 0);
-            vao.Unbind                ();
-
+            glClearColor          (245/255.0f, 245/255.0f, 250/255.0f, 1.0f);
+            glClear               (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            shader.Use            ();
+            shader.SetUniformMat4 ("cameraProjection", camera.GetCameraProjection ());
+            shader.SetUniformMat4 ("cameraView",       camera.GetCameraView       ());
+            model.Draw            (shader);
         EndDrawing    ();
     }
     CloseWindow ();
